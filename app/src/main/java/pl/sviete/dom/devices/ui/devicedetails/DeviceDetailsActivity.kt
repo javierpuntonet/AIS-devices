6feef95ebd96e035625462d7ddbbbaeb2e4e374c
Kamil B <kamilbrant@gmail.com>
2019-05-19 20:29:35 +0200

    remove webview, open device website on link

--- app/src/main/java/pl/sviete/dom/devices/ui/devicedetails/DeviceDetailsActivity.kt
+++ app/src/main/java/pl/sviete/dom/devices/ui/devicedetails/DeviceDetailsActivity.kt
@@ -1,14 +1,18 @@
 package pl.sviete.dom.devices.ui.devicedetails
 
 import android.app.AlertDialog
-import android.content.Context
-import android.content.res.Resources
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_device_details.*
 import pl.sviete.dom.devices.R
 import pl.sviete.dom.devices.db.AisDeviceEntity
 import java.lang.Exception
+import android.content.Intent
+import android.net.Uri
+import android.text.SpannableStringBuilder
+import android.text.Spanned
+import android.text.style.URLSpan
+import android.widget.TextView
 
 class DeviceDetailsActivity : AppCompatActivity(), DeviceDetailsView.View {
 
@@ -19,11 +23,10 @@ class DeviceDetailsActivity : AppCompatActivity(), DeviceDetailsView.View {
         setContentView(R.layout.activity_device_details)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        webView.settings.javaScriptEnabled = true
+        initTxtOpenWebSite()
 
         btn_save.setOnClickListener {
             try {
-
                 clearError()
                 presenter.saveView(txt_device_name.text.toString(), txt_device_ip.text.toString())
             }catch (e: Exception) {
@@ -42,8 +45,6 @@ class DeviceDetailsActivity : AppCompatActivity(), DeviceDetailsView.View {
     override fun showView(device: AisDeviceEntity) {
         txt_device_name.setText(device.name)
         txt_device_ip.setText(device.ip)
-
-        webView.loadUrl("http://" + device.ip)
     }
 
     private fun showQuestionForDelete() {
@@ -65,6 +66,21 @@ class DeviceDetailsActivity : AppCompatActivity(), DeviceDetailsView.View {
     private fun clearError(){
         txt_device_name.error = null
         txt_device_ip.error = null
+    }
+
+    private fun initTxtOpenWebSite() {
+        val ssb = SpannableStringBuilder()
+        ssb.append(txt_open_dev_website.text)
+        ssb.setSpan(URLSpan("#"), 0, ssb.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        txt_open_dev_website.setText(ssb, TextView.BufferType.SPANNABLE)
+        txt_open_dev_website.setOnClickListener {
+            val ip = txt_device_ip.text.toString()
+            if (ip.isNotEmpty()) {
+                val uri = Uri.parse("http://$ip")
+                val intent = Intent(Intent.ACTION_VIEW, uri)
+                startActivity(intent)
+            }
+        }
     }
 
     companion object {
